cedc004d589374658953b794a39469152010577f
Christophe Burckard <christopheburckard@yahoo.fr>
2022-01-15 12:59:05 +0100

    Initialisation du projet: pom, classe Principale et configuration de .gitignore

---
+++ src/main/java/fr/afpa/fermatscience/outilsitinerants/application/Principale.java
@@ -0,0 +1,9 @@
+package fr.afpa.fermatscience.outilsitinerants.application;
+
+public class Principale {
+
+    public static void main(String[] args) {
+	System.out.println("Bienvenue groupe 3 !");
+    }
+
+}
