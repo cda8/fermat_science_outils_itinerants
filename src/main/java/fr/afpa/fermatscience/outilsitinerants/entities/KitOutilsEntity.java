1a63f4dba3fb7763d8d1319f1b2710c1020afb79
Christophe Burckard <christopheburckard@yahoo.fr>
2022-01-15 13:18:43 +0100

    Création classe KitOutilsEntity

---
+++ src/main/java/fr/afpa/fermatscience/outilsitinerants/entities/KitOutilsEntity.java
@@ -0,0 +1,11 @@
+package fr.afpa.fermatscience.outilsitinerants.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "KitsOutils")
+@Table(name = "KitsOutils")
+
+public class KitOutilsEntity {
+
+}
